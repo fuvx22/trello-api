918efc65e2ba15195603b5604bb46d5ec3073753
fuvx22 <voxngocphu@gmail.com>
2024-01-16 20:11:46 +0700

    express route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,18 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoute } from '~/routes/v1/boardRoute'
+
+const Router = express.Router()
+
+// Check APIs v1 status
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs v1 are ready to use' })
+})
+
+// Board APIs
+Router.use('/boards', boardRoute)
+
+export const APIs_V1 = Router
