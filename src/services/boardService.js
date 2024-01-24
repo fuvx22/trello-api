fe367b51fbaf31beb531a67b61ad81c1de8ef3c0
fuvx22 <voxngocphu@gmail.com>
2024-01-24 20:48:22 +0700

    use aggregate to get details of board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -8,7 +8,7 @@ import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
-
+import { cloneDeep } from 'lodash'
 
 
 const createNew = async (reqBody) => {
@@ -35,8 +35,21 @@ const getDetails = async (boardId) => {
     if (!board) {
       throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
     }
+    // Clone deep board thành respone board để xử lý
+    const resBoard = cloneDeep(board)
+    // Đưa cards vào đúng column của nó
+    resBoard.columns.forEach(column => {
 
-    return board
+      // Object id của mongodb có hỗ trợ func equals để so sánh _id
+      column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
+
+      // Cách 2: dùng toString() của JS để đưa về string để so sánh
+      // column.cards = resBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
+    })
+    // Xóa mảng cards nằm cùng cấp mảng columns
+    delete resBoard.cards
+
+    return resBoard
   } catch (error) {
     throw error
   }
